ada664ba673de746ccff08759bf95f0445e4a0d2
Lluís <lluiscab@gmail.com>
2020-08-28 16:14:53 +0200

    Added cross compile script for Raspberry Pi Zero

---
+++ build/arm-musleabihf.sh
@@ -0,0 +1,13 @@
+# https://github.com/mmstick/cargo-deb/issues/99
+
+echo "Installing cargo-deb"
+cargo install cargo-deb
+
+echo Building binary on docker
+docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:arm-musleabihf cargo build --target arm-unknown-linux-musleabihf --release --features build-for-deb
+
+echo "Fixing permissions"
+sudo chmod 666 ./target/arm-unknown-linux-musleabihf/release/mpqtt
+
+echo "Packaging binary into .deb file"
+cargo deb --target arm-unknown-linux-musleabihf --no-build
